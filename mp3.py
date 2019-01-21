79f18b270ff390157eb1f96c6fc9b52887156c58
dannyavni <dannyavni@hotmail.com>
2019-01-21 13:16:05 -0600

    part 2: iterative fact

--- mp3.py
+++ mp3.py
@@ -5,3 +5,20 @@ import pandas as pd
 import time
 import random
 
+# recursuve facorial implemntation adapted retrieved from:
+# https://github.com/egonSchiele/grokking_algorithms/blob/master/02_selection_sort/python/01_selection_sort.py
+
+def recursive_fact(x):
+  if x == 1:
+    return 1
+  else:
+    return x * recusive_fact(x-1)
+	
+# iterative factorial solution u
+
+def iterative_fact(x):
+  fact = 1
+  for i in range(1,x+1):
+    fact = fact * i
+  return fact
+
